707b035361f5f08ac25bb15e319a0d623c0c1d3a
AyrtonVanGorp <r1056967@student.thomasmore.be>
2026-08-21 15:09:59 +0200

    database.py aangepast

--- database.py
+++ database.py
@@ -1,23 +1,25 @@
 import os
-import psycopg2
-from psycopg2.extras import RealDictCursor
+import psycopg
+from psycopg.rows import dict_row
 from dotenv import load_dotenv
 
 # Load variables from .env
 load_dotenv()
 
+
 def get_db_connection():
     """
     Creates a connection to the database.
-    We use RealDictCursor so that rows come back as
+    We use dict_row so that rows come back as
     {'column': 'value'} instead of (value, value).
     """
     try:
-        conn = psycopg2.connect(
+        conn = psycopg.connect(
             os.getenv("DATABASE_URL"),
-            cursor_factory=RealDictCursor
+            row_factory=dict_row
         )
         return conn
+
     except Exception as e:
         print(f"Error connecting to database: {e}")
         return None
